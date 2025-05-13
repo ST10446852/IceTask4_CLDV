ba914cb012d38383c92fb6f4b886eca698906d33
kiri173 <ST10446852@vcconnect.edu.za>
2025-05-13 11:20:13 +0200

    Added code
    Added code

---
+++ BlobQuickstart/Program.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Retrieve the connection string for use with the application. 
+string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+
+// Create a BlobServiceClient object 
+var blobServiceClient = new BlobServiceClient(connectionString);
+
+
+//Create a unique name for the container
+string containerName = "quickstartblobs" + Guid.NewGuid().ToString();
+
+// Create the container and return a container client object
+BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+
+
+// Create a local file in the ./data/ directory for uploading and downloading
+string localPath = "data";
+Directory.CreateDirectory(localPath);
+string fileName = "quickstart" + Guid.NewGuid().ToString() + ".txt";
+string localFilePath = Path.Combine(localPath, fileName);
+
+// Write text to the file
+await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+// Get a reference to a blob
+BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+// Upload data from the local file, overwrite the blob if it already exists
+await blobClient.UploadAsync(localFilePath, true);
+
